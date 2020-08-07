9b9c4b01c2f6c1bb9981c8f0caa022b9aa81f6a2
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-08-07 17:12:54 +0900

    18.関数化

--- memo/notes.js
+++ memo/notes.js
@@ -2,25 +2,36 @@ console.log("notes.js開始");
 
 const fs = require("fs");
 
+let fetchNotes = () => {
+  try {
+    let notesString = fs.readFileSync("notes-data.json");
+    return JSON.parse(notesString);
+
+  } catch(e){
+    return [];
+  };
+  
+};
+
+let saveNotes = notes => {
+  fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+};
+
+
 let addNote = (title, body) => {
-  let notes = [];
+  let notes = fetchNotes();
   let note = {
     title,
     body
   };
 
-  try {
-    let notesString = fs.readFileSync("notes-data.json");
-    notes = JSON.parse(notesString);  
-  } catch(e){
 
-  };
-  // 「note.title」
+
   let duplicatedNotes = notes.filter(note => note.title === title) 
 
   if (duplicatedNotes.length === 0 ){
     notes.push(note);
-    fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+    saveNotes(notes);
   
   };
 
