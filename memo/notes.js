b145ae47b24cb2437f1f5d6d0d2811fe53ede4b3
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-08-07 18:41:26 +0900

    20.削除

--- memo/notes.js
+++ memo/notes.js
@@ -25,15 +25,11 @@ let addNote = (title, body) => {
     body
   };
 
-
-
   let duplicatedNotes = notes.filter(note => note.title === title) 
-
   if (duplicatedNotes.length === 0 ){
     notes.push(note);
     saveNotes(notes);
     return note;
-  
   };
 
 };
@@ -47,7 +43,13 @@ let readNote = title => {
 };
 
 let removeNote = title => {
-  console.log("メモ削除", title);
+  // 既存のデータを取得
+  let notes = fetchNotes();
+  // 因数を元に検索し、見つかったら削除
+  let filteredNotes = notes.filter(note => note.title !== title);
+  // 削除後のデータを削除
+  saveNotes(filteredNotes);
+  return notes.length !== filteredNotes.length;
 };
 
 module.exports = {
